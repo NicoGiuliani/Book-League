8386e2d76c669e276d9524fa5c30f762561460fd
Nico Julian <NicoGiuliani@users.noreply.github.com>
2023-09-22 10:24:52 -0700

    Got book search results page to show multiple results

--- app/views.py
+++ app/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
+from urllib.parse import urlencode
 from .forms import UserSignupForm
 import requests
 from dotenv import load_dotenv
@@ -19,31 +20,42 @@ def AddBookView(request):
     return render(request, "addbook.html")
 
 def SearchResultsView(request):
-    query_title = request.GET['title']
-    query_author = request.GET['author']
-    api_key = os.environ.get('API_KEY')
+    base_url = "https://www.googleapis.com/books/v1/volumes?"
+    params = {'q': request.GET['title'], 'inauthor': request.GET['author'], 'key': os.environ.get('API_KEY')}
 
     try:
-        response = requests.get(f"https://www.googleapis.com/books/v1/volumes?q={query_title}+inauthor:{query_author}&key={api_key}", params=request.GET)
+        response = requests.get(base_url, params=params)
         response_json = response.json()
+        bookList = []
+        print(response_json['totalItems'])
 
-        title = response_json['items'][0]['volumeInfo']['title']
-        author = response_json['items'][0]['volumeInfo']['authors'][0]
-        description = response_json['items'][0]['volumeInfo']['description']
-        thumbnail = response_json['items'][0]['volumeInfo']['imageLinks']['thumbnail']
-        language = response_json['items'][0]['volumeInfo']['language']
-        bookInfo = {
-            "title": title,
-            "author": author,
-            "description": description,
-            "thumbnail": thumbnail,
-            "language": language
-        }
-        print(bookInfo["title"])
-        return render(request, "searchResults.html", { "searchResult": bookInfo })
+        if response_json['totalItems'] > 0:
+            for i in range(response_json['totalItems']):
+                print("i:", i)
+                volumeInfo = response_json['items'][i]['volumeInfo']
+                print(volumeInfo)
+                bookInfo = {
+                    "title": volumeInfo['title'],
+                    "author": volumeInfo['authors'][0],
+                    "description": volumeInfo['description'],
+                    "thumbnail": volumeInfo['imageLinks']['thumbnail'] if ('imageLinks' in volumeInfo and 'thumbnail' in volumeInfo['imageLinks']) else None,
+                    "language": volumeInfo['language']
+                }
+                print("got book info")
+                bookList.append(bookInfo)
+                print("bookList length:", len(bookList))
+                if len(bookList) == 5:
+                    print("Reached length 5")
+                    print("final bookList:", bookList)
+                    break
+            print("rendering...")
+            return render(request, "searchResults.html", { "searchResult": bookList })
+        else:
+            print("No results found")
+            return render(request, "searchResults.html", { "searchResult": None })
+        
     except (Exception):
-        return render(request, "searchResults.html", { "searchResult": None })
-    
+        return render(request, "error.html")
     
 
 class SignUpView(generic.CreateView):
